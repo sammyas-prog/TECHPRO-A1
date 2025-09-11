91c1e0b0ad3766c99c6fe80eb1a49c18487ba78e
MohammadAshrawiSAmporna <mohammadashrawi1011@gmail.com>
2025-09-11 22:57:38 +0800

    Update Try.java

--- Try.java
+++ Try.java
@@ -1,4 +1,4 @@
-public class Try
-public statistic void main(String[] args) {
- System.out.println("nice");
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("SEATWORKELEVEN");
 }
